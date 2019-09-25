150cf0467cd589ec442ab0c299867f3fcb40e75e
Yosi <7910558+tochiji@users.noreply.github.com>
2019-09-26 00:54:37 +0900

    [進捗] 条件付きレンダー：論理&&演算子によるインラインif

--- src/index.js
+++ src/index.js
@@ -69,9 +69,30 @@ class LoginControl extends React.Component{
   }
 }
 
+// ReactDOM.render(
+//   <LoginControl />,document.getElementById('root')
+// )
+
+function Mailbox(props){
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
+  )
+}
+
+// const messages = ['React', 'Re: React', 'Re:Re: React'];
+const messages = [];
 ReactDOM.render(
-  <LoginControl />,document.getElementById('root')
-)
+  <Mailbox unreadMessages={messages} />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
