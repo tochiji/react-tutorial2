5cdb845f2985328b2c8194bea3cb2f0e319c1633
Yosi <7910558+tochiji@users.noreply.github.com>
2019-09-27 02:23:51 +0900

    [進捗] リストと key：key は兄弟要素の中で一意であればよい

--- src/index.js
+++ src/index.js
@@ -3,15 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-const numbers = [1,2,3,4,5];
-const listItems = numbers.map((n) =>
-  <li>{n}</li>
-)
+function Blog(props){
+  const sidebar = (
+    <ul>
+      {props.posts.map((post)=>
+        <li key={post.id}>{post.title}</li>
+      )}
+    </ul>
+  )
+  const content = props.posts.map((post)=>
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+  return (
+  <div>
+    {sidebar}
+    <hr />
+    {content}
+  </div>
+  );
+}
 
-ReactDOM.render(
-  <ul>{listItems}</ul>,
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+]
+
+ReactDOM.render(<Blog posts={posts} />,
   document.getElementById('root')
-)
+  )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
