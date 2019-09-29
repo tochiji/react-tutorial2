15f240a52e618fd71dad6bcefc7e416b8e863b63
Yosi <7910558+tochiji@users.noreply.github.com>
2019-09-30 01:04:54 +0900

    [進捗] stateのリフトアップ：完了 あとちょっとでMAIN CONCEPTSが終わる…

--- src/index.js
+++ src/index.js
@@ -3,140 +3,90 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
 
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { value: '' }
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>
   }
-  handleChange = event => {
-    this.setState({ value: event.target.value })
-  }
-  handleSubmit = event => {
-    alert('A name was submitted: ' + this.state.value)
-  }
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    )
-  }
+  return <p>The water would not boil.</p>
 }
 
-class EssayForm extends React.Component {
+function toCelsius(faherenheit) {
+  return ((faherenheit - 32) * 5) / 9
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature)
+  if (Number.isNaN(input)) {
+    return ''
+  }
+  const output = convert(input)
+  const rounded = Math.round(output * 100) / 1000
+  return rounded.toString()
+}
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit',
+}
+
+class Calclator extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      value: 'Please write an essay about your favorite DOM element.',
-      texts: [],
-    }
+    this.state = { temperature: '', scale: 'c' }
   }
-  handleChange = event => {
-    this.setState({ value: event.target.value })
+
+  handleCelsiusChange = temperature => {
+    this.setState({ scale: 'c', temperature })
   }
-  handleSubmit = event => {
-    const texts = this.state.texts.slice()
-    texts.push(this.state.value)
-    this.setState({ texts: texts })
-    alert('An essay was submitted: ' + this.state.value)
-    event.preventDefault()
+
+  handleFahrenheitChange = temperature => {
+    this.setState({ scale: 'f', temperature })
   }
 
   render() {
+    const scale = this.state.scale
+    const temperature = this.state.temperature
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature
+    const fahrenheit = scale == 'c' ? tryConvert(temperature, toFahrenheit) : temperature
+
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Essay:
-            <textarea value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        {this.state.texts.map(t => (
-          <div>{t}</div>
-        ))}
+        <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleCelsiusChange} />
+        <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitChange} />
+
+        <BoilingVerdict celsius={parseFloat(celsius)} />
       </div>
     )
   }
 }
 
-class FlavorForm extends React.Component {
+class TemperatureInput extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { value: ['ハズレ', 'coconutだね'] }
+    this.handleChange = this.handleChange.bind(this)
   }
-  handleChange = event => {
-    const value = this.state.value.splice()
-    value.push(event.target.value)
-    this.setState({ value: value })
-  }
-  handleSubmit = event => {
-    alert('あなたの好きな味は' + this.state.value)
-    event.preventDefault()
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value)
   }
 
   render() {
+    const temperature = this.props.temperature
+    const scale = this.props.scale
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Pick your favorite flavor:
-          <select multiple value={this.state.value} onChange={this.handleChange}>
-            <option value="ハズレ">Grapefruit</option>
-            <option value="limeやね">Lime</option>
-            <option value="coconutだね">Coconut</option>
-            <option value="mangoだね">Mango</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
     )
   }
 }
 
-class Reservation extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isGoing: true,
-      numberOfGuests: 2,
-    }
-  }
-  handleInputChange = event => {
-    const target = event.target
-    const value = target.type === 'checkbox' ? target.checked : target.value
-    const name = target.name
-
-    this.setState({
-      [name]: value,
-    })
-  }
-  render() {
-    return (
-      <form>
-        <label>
-          Is going:
-          <input name="isGoing" type="checkbox" checed={this.state.isGoing} onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Number of guests:
-          <input
-            name="numberOfGuests"
-            type="number"
-            value={this.state.numberOfGuests}
-            onChange={this.handleInputChange}
-          />
-        </label>
-      </form>
-    )
-  }
-}
-
-ReactDOM.render(<Reservation />, document.getElementById('root'))
+ReactDOM.render(<Calclator />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
