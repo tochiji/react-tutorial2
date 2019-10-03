757706ad4063a71acf195052ce8f620e237adca1
Yosi <7910558+tochiji@users.noreply.github.com>
2019-10-04 01:03:24 +0900

    [進捗] React hooks：2.フック早わかり

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
 
-function Repeat(props){
-  const items = []
-  for(let i=0;i<props.numTimes;i++){
-    items.push(props.children(i))
-  }
-  return <div>{items}</div>
-}
+function Example(){
+  const [count,setCount] = useState(10);
 
-function ListOfTenThings(){
-  return (
-    <Repeat numTimes={10}>
-      {i => <div key={i}>This is item {i} in the list</div>}
-    </Repeat>
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return(
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={()=> setCount(count+1)}>
+        Click me
+      </button>
+    </div>
   )
 }
 
 const mount = document.getElementById('root')
-ReactDOM.render(<ListOfTenThings />, mount)
+ReactDOM.render(<Example />, mount)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
